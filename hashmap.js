c9df3b1cc914ab97807e8a0ebd5c7a063cb1380d
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-11-26 23:02:27 +0200

    Make hash func return a hash code

--- hashmap.js
+++ hashmap.js
@@ -8,5 +8,12 @@ function hashMap() {
 
 // Method
 function hash (key) {
-  
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < key.length; i++) {
+    hashCode = primeNumber * hashCode + key.charCodeAt(i);
+  }
+
+  return hashCode;
 }
